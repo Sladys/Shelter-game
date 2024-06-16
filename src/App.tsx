e9ea7410d6087314d0aa7180c4cdd4338aab4aec
Sladys <isaulslady@gmail.com>
2024-06-16 16:24:54 +0300

    213

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="text-red-900 bg-gray-700 p-20 underline">Hello Ilya</p>
+        <p className="bg-gray-700 p-20 text-red-900 underline">Hello Ilya</p>
       </header>
     </div>
   );
 }
 
 export default App;
+    
