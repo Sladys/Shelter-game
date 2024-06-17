dbbc6106c9cbd1516c8f6976fdc94fc7831c4c59
Sladys <isaulslady@gmail.com>
2024-06-17 16:16:17 +0300

    Рефакторинг

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PlayerCardList from "../game/playerCardList";
 import { GameScreen } from "../../pages/gameScreen/gameScreen";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<PlayerCardList />} />
-      </Routes>
       <Routes>
         <Route path="/game" element={<GameScreen />} />
       </Routes>
