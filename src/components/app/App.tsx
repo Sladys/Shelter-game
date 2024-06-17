5a99b015656fc46d8ba0b3130c3d130ea24e9744
Sladys <isaulslady@gmail.com>
2024-06-17 18:13:46 +0300

    123

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GameScreen } from "../../pages/gameScreen/gameScreen";
+import { Apocalypses, BunkersData, CardsInfo } from "../../types/types";
 
-function App() {
+type AppProps = {
+  apocalypses: Apocalypses;
+  bunkersData: BunkersData;
+  cardsInfo: CardsInfo;
+};
+
+function App({ cardsInfo, apocalypses, bunkersData }: AppProps) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/game" element={<GameScreen />} />
+        <Route
+          path="/game"
+          element={
+            <GameScreen
+              apocalypses={apocalypses}
+              bunkersData={bunkersData}
+              cardsInfo={cardsInfo}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/components/game/playerCardList.tsx
+++ src/components/game/playerCardList.tsx
@@ -1,10 +1,15 @@
+import { CardsInfo } from "../../types/types";
 import PlayerCard from "./playerCard";
 
-function PlayerCardList(): JSX.Element {
+type PlayerCardListProps = {
+  cardsInfo: CardsInfo;
+};
+
+function PlayerCardList({cardsInfo} : PlayerCardListProps): JSX.Element {
   return (
-    <ul className="container mx-auto flex list-none flex-wrap">
+    <ul className="container mx-auto flex list-none flex-wrap justify-between">
       {Array.from({ length: 10 }, (_, id) => (
-        <PlayerCard key={id} />
+        <PlayerCard key={id} cardInfo={cardsInfo[id]} />
       ))}
     </ul>
   );
