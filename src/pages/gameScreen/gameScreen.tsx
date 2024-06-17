4725590b65b324d9f5fac7c0bbce68183aa2a2d8
yukishidzu <i@yukishidzu.ru>
2024-06-17 16:50:25 +0300

    разбил моки на компоненты

--- src/pages/gameScreen/gameScreen.tsx
+++ src/pages/gameScreen/gameScreen.tsx
@@ -1,15 +1,16 @@
 import GameInfo from "../../components/game/gameInfo";
-import { apocalypseData, bunkerData } from "../../mocks/bunkerData";
+import { bunkerData } from "../../mocks/bunkerData";
+import { apocalypseData } from "../../mocks/apocalypseData";
 import GameMenu from "../../components/game/gameMenu";
 import PlayerCardList from "../../components/game/playerCardList";
 
 function GameScreen(): JSX.Element {
   return (
-      <main className="container mx-auto flex h-screen items-center flex-col">
-        <GameInfo apocalypse={apocalypseData} bunker={bunkerData} />
-        <GameMenu />
-        <PlayerCardList />
-      </main>
+    <main className="container mx-auto flex h-screen flex-col items-center">
+      <GameInfo apocalypse={apocalypseData} bunker={bunkerData} />
+      <GameMenu />
+      <PlayerCardList />
+    </main>
   );
 }
 
