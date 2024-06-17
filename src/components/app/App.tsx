54d8b2c5482c8fa0f7c9d9a1cdf9afd780acfaf2
Sladys <isaulslady@gmail.com>
2024-06-17 12:36:06 +0300

    Добавил компоненты карточек игрока

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PlayerCardList from "../game/playerCardList";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route index element={<PlayerCardList />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/game/playerCard.tsx
+++ src/components/game/playerCard.tsx
@@ -1,7 +1,25 @@
-function PlayerInfo(): JSX.Element {
+import { Fragment } from "react";
+
+function PlayerCard(): JSX.Element {
   return (
-    <></>
+    <Fragment>
+      <li className="m-6 w-2/12 bg-green-300">
+        <ul className="list-none p-2">
+          <li>Специальность</li>
+          <li>Пол, возраст</li>
+          <li>Хобби</li>
+          <li>Багаж</li>
+          <li>Фобия</li>
+          <li>Состояние здоровья</li>
+          <li>Доп. информация</li>
+        </ul>
+        <ul className="list-none border-t-2 border-indigo-900 p-2">
+          <li>Доп Возможность №1</li>
+          <li>Доп Возможность №2</li>
+        </ul>
+      </li>
+    </Fragment>
   );
 }
 
-export default PlayerInfo;
+export default PlayerCard;
