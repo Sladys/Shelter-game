0f063f0e2d6685827b366474682e6c37cab850e5
Sladys <isaulslady@gmail.com>
2024-06-17 18:38:55 +0300

    123

--- src/components/game/playerCardList.tsx
+++ src/components/game/playerCardList.tsx
@@ -1,4 +1,5 @@
 import { CardsInfo } from "../../types/types";
+import { getRandomElement } from "../../utils";
 import PlayerCard from "./playerCard";
 
 type PlayerCardListProps = {
@@ -9,7 +10,7 @@ function PlayerCardList({cardsInfo} : PlayerCardListProps): JSX.Element {
   return (
     <ul className="container mx-auto flex list-none flex-wrap justify-between">
       {Array.from({ length: 10 }, (_, id) => (
-        <PlayerCard key={id} cardInfo={cardsInfo[id]} />
+        <PlayerCard key={id} cardInfo={getRandomElement(cardsInfo)} />
       ))}
     </ul>
   );
