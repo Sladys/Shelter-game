4c3452454db6986bf9797711868a7982eccb2295
yukishidzu <i@yukishidzu.ru>
2024-06-17 17:03:58 +0300

    добавил моки

--- src/pages/gameScreen/gameScreen.tsx
+++ src/pages/gameScreen/gameScreen.tsx
@@ -1,13 +1,14 @@
 import GameInfo from "../../components/game/gameInfo";
 import { bunkerData } from "../../mocks/bunkerData";
-import { apocalypseData } from "../../mocks/apocalypseData";
+import { generateApocalypses } from "../../mocks/apocalypseData";
 import GameMenu from "../../components/game/gameMenu";
 import PlayerCardList from "../../components/game/playerCardList";
+import { getRandomElement } from "../../utils";
 
 function GameScreen(): JSX.Element {
   return (
     <main className="container mx-auto flex h-screen flex-col items-center">
-      <GameInfo apocalypse={apocalypseData} bunker={bunkerData} />
+      <GameInfo apocalypse={getRandomElement(generateApocalypses)} bunker={bunkerData} />
       <GameMenu />
       <PlayerCardList />
     </main>
