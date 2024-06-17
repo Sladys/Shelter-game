69665f2436e6ec61b175245caca1f2856652e4fe
yukishidzu <i@yukishidzu.ru>
2024-06-17 17:03:58 +0300

    добавил моки

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type Apocalypse = {
+  type: string;
+  description: string;
+  population: string;
+  period: string;
+};
+
+export type BunkerData = {
+  description: string;
+  food: string;
+  structure: string;
+  additional: string;
+};
