2446a969a0f131770d3b48de2aa41e2b3f5dc267
Sladys <isaulslady@gmail.com>
2024-06-17 18:13:46 +0300

    123

--- src/components/game/playerCard.tsx
+++ src/components/game/playerCard.tsx
@@ -1,25 +1,48 @@
 import { Fragment } from "react";
+import { CardInfo } from "../../types/types";
 
-function PlayerCard(): JSX.Element {
+type PlayerCardProps = {
+  cardInfo: CardInfo;
+};
+
+function PlayerCard({ cardInfo }: PlayerCardProps): JSX.Element {
   return (
     <Fragment>
-      <li className="m-6 w-2/12 bg-gray-100">
+      <li className="m-6 w-[320px] bg-gray-100">
         <div className="flex p-2">
           <span className="inline-block h-20 w-20 bg-red-200">avatar</span>
           <span className="ml-8 inline-block">nick</span>
         </div>
         <ul className="list-none p-2">
-          <li>Специальность</li>
-          <li>Пол, возраст</li>
-          <li>Хобби</li>
-          <li>Багаж</li>
-          <li>Фобия</li>
-          <li>Состояние здоровья</li>
-          <li>Доп. информация</li>
+          <li>
+            Специальность: <span className="ml-2">{cardInfo.profession}</span>
+          </li>
+          <li>
+            Пол, возраст: <span className="ml-2">{cardInfo.bio}</span>
+          </li>
+          <li>
+            Хобби: <span className="ml-2">{cardInfo.hobby}</span>
+          </li>
+          <li>
+            Багаж: <span className="ml-2">{cardInfo.inventory}</span>
+          </li>
+          <li>
+            Фобия: <span className="ml-2">{cardInfo.phobia}</span>
+          </li>
+          <li>
+            Состояние здоровья: <span className="ml-2">{cardInfo.health}</span>
+          </li>
+          <li>
+            Доп. информация:<span className="ml-2">{cardInfo.additional}</span>
+          </li>
         </ul>
         <ul className="list-none border-t-2 border-indigo-900 p-2">
-          <li>Доп Возможность №1</li>
-          <li>Доп Возможность №2</li>
+          <li>
+            Спец Возможность №1: <span className="ml-2">{cardInfo.actionFirst}</span>
+          </li>
+          <li>
+            Спец Возможность №2: <span className="ml-2">{cardInfo.actionSecond}</span>
+          </li>
         </ul>
       </li>
     </Fragment>
